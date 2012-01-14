21a9b847c05c66221209708d87d6b3e6f94fa21d
stlifey <stlifey@gmail.com>
2012-01-15 02:02:06 +0800

    interacvite pack bootimg tool

--- Kernel.mk
+++ Kernel.mk
@@ -1,4 +1,13 @@
+#!/bin/bash
+PS3="Choose (1-3):"
+echo "Choose from the list below."
+select name in CyanogenMod6 CyanogenMod7 StockEclair
+do
+	break
+done
+echo "You chose $name."
+
 cp arch/arm/boot/zImage bootimg/
 cd bootimg/
-./repack-bootimg.pl zImage cm/ \#$(cat $(pwd)/../.version)-$(stat -c %y $(pwd)/../arch/arm/boot/zImage | cut -b1-10).img
+./repack-bootimg.pl zImage $name/ \#$(cat $(pwd)/../.version)-$name-$(stat -c %y $(pwd)/../arch/arm/boot/zImage | cut -b1-10).img
 echo "Pack boot image done!"
