744ac120260c2ea50864c1b11484c691750b8c11
fentensoft <fentensoft@163.com>
2012-01-17 17:09:04 +0800

    fix

--- bootimg/repack-bootimg.pl
+++ bootimg/repack-bootimg.pl
@@ -12,12 +12,12 @@ die $usage unless $ARGV[0] && $ARGV[1] && $ARGV[2];
 
 chdir $ARGV[1] or die "$ARGV[1] $!";
 
-system ("find . | cpio -o -H newc | gzip > $dir/ramdisk-repack.cpio.gz");
+system ("find . | cpio -o -H newc | lzma > $dir/ramdisk-repack.cpio.lzma");
 
 chdir $dir or die "$ARGV[1] $!";;
 
-system ("./mkbootimg --kernel $ARGV[0] --ramdisk ramdisk-repack.cpio.gz -o $ARGV[2]");
+system ("./mkbootimg --kernel $ARGV[0] --ramdisk ramdisk-repack.cpio.lzma -o $ARGV[2]");
 
-unlink("ramdisk-repack.cpio.gz") or die $!;
+unlink("ramdisk-repack.cpio.lzma") or die $!;
 
 print "\nrepacked boot image written at $ARGV[1]-repack.img\n";
